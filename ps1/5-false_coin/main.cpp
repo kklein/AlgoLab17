598888fdd3f8c3650a18625e4551595a187cf395
kkleindev <kleinkevin.lu@gmail.com>
2017-09-22 16:06:57 +0000

    dirty solution for exercise 5

--- ps1/5-false_coin/main.cpp
+++ ps1/5-false_coin/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -11,26 +12,23 @@ void set_values(vector<int>& coins, vector<int> v, int value) {
   if (value == 1) {
     for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
       coins[*it - 1] = 1;
-      if (*it-1 == 182) {cout << "183 has been set to 1" << endl;}
     }
     return;
   }
   int actual_value;
   for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
     actual_value = coins[*it - 1];
-    
     if (actual_value == 0) {
       coins[*it - 1] = value;
-      if (*it-1 == 182) {cout << "183 has been set to"; cout << value << endl;}
     } else if (actual_value == 1 || (actual_value == 2 && value == 3) || 
         (actual_value == 3 && value == 2)) {
       coins[*it - 1] = 1;
-      if (*it-1 == 182) {cout << "183 has been set to 1"<< endl;}
     }
   }
 }
 
 int main() {
+  ios_base::sync_with_stdio(false);
   int n_test_cases;
   cin >> n_test_cases;
   for (int test_case = 0; test_case < n_test_cases; test_case++) {
@@ -38,6 +36,7 @@ int main() {
     int n_weighings;
     int n_coins_per_side;
     char op;
+    set<int> remaining_coins;
     cin >> n_coins;
     cin >> n_weighings;
     vector<int> coins(n_coins);
@@ -60,10 +59,19 @@ int main() {
         case '<' :
           set_values(coins, left, 2);
   	  set_values(coins, right, 3);
+	  for (int i = 0; i < coins.size(); i++) {remaining_coins.insert(i);}
+          for (int i = 0; i < left.size(); i++) {remaining_coins.erase(left[i] - 1);}
+          for (int i = 0; i < right.size(); i++) {remaining_coins.erase(right[i] - 1);}
+          for (set<int>::iterator it = remaining_coins.begin(); it != remaining_coins.end(); ++it) { 	  coins[*it] = 1;}
           break;
         case '>' :
           set_values(coins, left, 3);
           set_values(coins, right, 2);
+	  for (int i = 0; i < coins.size(); i++) {remaining_coins.insert(i);}
+          for (int i = 0; i < left.size(); i++) {remaining_coins.erase(left[i] - 1);}
+          for (int i = 0; i < right.size(); i++) {remaining_coins.erase(right[i] - 1);}
+          for (set<int>::iterator it = remaining_coins.begin(); it != remaining_coins.end(); ++it) {
+              coins[*it] = 1;}
           break;
       }
     }
@@ -71,9 +79,9 @@ int main() {
     int candidate;
     int candidate_count = 0;
     for (int i = 0; i < n_coins; i++) {
-      cout << i;
-      cout << ':';
-      cout << coins[i] << endl;
+      //cout << i;
+      //cout << ':';
+      //cout << coins[i] << endl;
       if (coins[i] != 1) {
         candidate = i + 1;
         candidate_count++;
@@ -81,14 +89,14 @@ int main() {
     }
 
     if (candidate_count == 1) {
-      cout << candidate;
+      cout << candidate << endl;
     } else {
-      cout << 0;
+      cout << 0 << endl;
     }
-    cout << "number of candidates" << endl;
-    cout << candidate_count << endl;
-    cout << "most recent candidate" << endl;
-    cout << candidate << endl;
+    //cout << "number of candidates" << endl;
+    //cout << candidate_count << endl;
+    //cout << "most recent candidate" << endl;
+    //cout << candidate << endl;
 
   }
   return 0;
