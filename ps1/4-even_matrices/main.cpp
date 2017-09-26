f96324ef99349471aacb752d4f07faa82139a266
kkleindev <kleinkevin.lu@gmail.com>
2017-09-26 14:25:29 +0200

    fix counting bug in even matrices

--- ps1/4-even_matrices/main.cpp
+++ ps1/4-even_matrices/main.cpp
@@ -19,48 +19,51 @@ int main() {
       }
     }
 
+    int even_count = 0;
+
     // Build sum matrix.
     int offset;
     vector<vector<int> > sums(size, vector<int>(size));
     sums[0][0] = matrix[0][0];
-    for (int j = 0; j < size; j++) {
+    if (sums[0][0] % 2 == 0) {
+      even_count++;
+    }
+    for (int j = 1; j < size; j++) {
       sums[0][j] = sums[0][j-1] + matrix[0][j];
+      if (sums[0][j] % 2 == 0) {
+        even_count++;
+      }
     }
     for (int i = 1; i < size; i++) {
       sums[i][0] = sums[i-1][0] + matrix[i][0];
+      if (sums[i][0] % 2 == 0) {
+        even_count++;
+      }
       for (int j = 1; j < size; j++) {
         offset = sums[i-1][j] - sums[i-1][j-1];
         sums[i][j] = sums[i][j-1] + offset + matrix[i][j];
+        if (sums[i][j] % 2 == 0) {
+          even_count++;
+        }
       }
     }
 
     // Count even sum differences.
-    int even_count = 0;
     int i1;
     int j1;
     int sum;
     for (int i2 = 0; i2 < size; i2++) {
       for (int j2 = 0; j2 < size; j2++) {
-        if (sums[i2][j2] % 2 == 0 && sums[i2][j2] != matrix[i2][j2]) {
-          even_count++;
-          //cout << "plain simple even: ";
-          //cout << i2;
-          //cout << j2 << endl;
-        }
         for (i1 = 0; i1 <= i2; i1++) {
           for (j1 = 0; j1 <= j2; j1++) {
             if ((i1 != i2 && j1 != j2)) {
               sum = sums[i2][j2] - sums[i2][j1] - sums[i1][j2] + sums[i1][j1];
               if (sum % 2 == 0) {
-                //cout << "combination: ";
-                //cout << i1; cout << j1; cout << i2; cout << j2 << endl;
                 even_count++;
               }
             } else if (i1 != i2 || j1 != j2) {
               sum = sums[i2][j2] - sums[i1][j1];
               if (sum % 2 == 0) {
-                //cout << "simple computation: ";
-                //cout << i1; cout << j1; cout << i2; cout << j2 << endl;
                 even_count++;
               }
             }
