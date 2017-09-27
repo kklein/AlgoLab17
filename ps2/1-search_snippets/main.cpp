33e8358fa94b6d753a1085cb2e2734f7c78a8e67
kkleindev <kleinkevin.lu@gmail.com>
2017-09-27 23:29:22 +0200

    iteration on search snippets exercise

--- ps2/1-search_snippets/main.cpp
+++ ps2/1-search_snippets/main.cpp
@@ -46,33 +46,42 @@ int main() {
       pos = positions[i];
       word = query[pos];
       if (word_selection[word] == -1) {
-        cout << "found word " << word << " for the first time at pos: " << pos << endl;
         words_found++;
       }
       if (word_selection[word] == left_most_pointer) {
-        left_most_pointer++;
-        cout << "visited: " << word << endl;
-        cout << "left most pointer: " << left_most_pointer << endl;
+        // TODO(kevinkle): make this more efficient
+        int new_left = n_positions;
+        for (int w = 0; w < n_words; w++) {
+          if (w != word) {
+            new_left = min(new_left, word_selection[w]);
+          }
+        }
+        left_most_pointer = new_left;
       }
       word_selection[word] = i;
       if (words_found == n_words) {
         distance = pos - positions[left_most_pointer] + 1;
+        cout << "left position: " << positions[left_most_pointer] << endl;
+        cout << "right position: " << pos << endl;
         cout << "current distance: " << distance << endl;
         min_distance = min(min_distance, distance);
         cout << "min distance: " << min_distance << endl;
       }
     }
     while (left_most_pointer < n_positions) {
-      word = query[left_most_pointer];
+      word = query[positions[left_most_pointer]];
       word_selection[word] = -1;
       words_found--;
       left_most_pointer++;
-      if (word_selection[query[left_most_pointer]] == -1) {
-        word_selection[query[left_most_pointer]] == left_most_pointer;
+      if (word_selection[query[positions[left_most_pointer]]] == -1) {
+        cout << "found " << query[positions[left_most_pointer]] << " again at position " << positions[left_most_pointer] << endl;
+        word_selection[query[positions[left_most_pointer]]] == left_most_pointer;
         words_found++;
       }
       if (words_found == n_words) {
-        distance = n_positions - 1 - left_most_pointer + 1;
+        cout << "left position: " << positions[left_most_pointer] << endl;
+        cout << "right position: " << positions[n_positions - 1] << endl;
+        distance = n_positions - 1 - positions[left_most_pointer] + 1;
         cout << "current distance: " << distance << endl;
         min_distance = min(min_distance, distance);
         cout << "min distance: " << min_distance << endl;
