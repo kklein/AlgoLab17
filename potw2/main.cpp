f06c437aae9d9f17b8da3610d42a7548c50c4fa2
kkleindev <kleinkevin.lu@gmail.com>
2017-10-03 11:54:00 +0200

    solve potw2 in a very hacky fashion

--- potw2/main.cpp
+++ potw2/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <math.h>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -36,37 +36,58 @@ int main() {
     vector<pair<int, int> > balls(n_balls);
     for (int ball = 0; ball < n_balls; ball++) {
       cin >> delay;
-      balls[balls] = make_pair(ball, delay);
-      data[ball] = make_pair(ball, delay);
+      pair<int, int> p = make_pair(ball, delay);
+      balls[ball] = p;
+      data[ball] = p;
     }
     sort(data.begin(), data.end(), compare);
-    for (int i = 0; i < n_balls; i++) {
-      cout << data[i].first << " : " << data[i].second << endl;
-    }
+    //for (int i = 0; i < n_balls; i++) {
+    //  cout << data[i].first << " : " << data[i].second << endl;
+    //}
     int i = 0;
     bool result = true;
     int taken_count = 0;
     while (result && taken_count < n_balls && i < n_balls) {
+      //cout << "#taken: " << taken_count << endl;
+      //cout << "i: " << i << endl;
       pair<int, int> p = data[i];
-      if (taken[p.first] > 1) {
+      if (taken[p.first] > 0) {
         i++;
       } else {
-        if (p.second > taken_count) {
-          result = false;
-        } else {
-          queue<pair<int, int> > dependents;
-          dependents.push(p);
-          while (not dependents.empty()) {
-            v = dependents.front();
-            dependents.pop();
-            if (taken[v.first] == 0 ) {
-              if (v.second > taken_count) {
-                taken[v.first] = 1;
-                taken_count++;
-                if (taken[2*j + 1] < n_balls) {
-                  dependents.push(balls[2*j + 1]);
-                  dependents.push(balls[2*j + 2]);
+        stack<pair<int, int> > dependents;
+        dependents.push(p);
+        while (not dependents.empty()) {
+          pair<int, int> p = dependents.top();
+          dependents.pop();
+          if (taken[p.first] == 0 ) {
+            int child_pos = 2 * p.first + 1;
+            if (child_pos < n_balls) {
+              dependents.push(p);
+              if (taken[child_pos] == 0) {
+                dependents.push(balls[child_pos]);
+              }
+              if (taken[child_pos + 1] == 0) {
+                dependents.push(balls[child_pos+ 1]);
+              }
+              if (taken[child_pos] > 0 and taken[child_pos + 1] > 0) {
+                p = dependents.top();
+                dependents.pop();
+                if (p.second > taken_count) {
+                  //cout << "take " << p.first << " with ti: " << p.second << " at time: " << taken_count
+                  //    << endl;
+                  taken[p.first] = 1;
+                  taken_count++;
+                } else {
+                  result = false;
                 }
+              }
+            } else {
+              if (p.second > taken_count) {
+                //cout << "take " << p.first << " with ti: " << p.second << " at time: " << taken_count
+                //    << endl;
+                taken[p.first] = 1;
+                taken_count++;
+
               } else {
                 result = false;
               }
@@ -75,6 +96,7 @@ int main() {
         }
       }
     }
+    cout << (result ? "yes" : "no") << endl;
   }
   return 0;
 }
