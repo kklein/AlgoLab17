3693909f818f27e7f2f6c0d5fdfb9754e69529cc
kkleindev <kleinkevin.lu@gmail.com>
2017-11-23 14:19:36 +0100

    Fix ps7's exercise 2 by declaring the linear program per test case instead of only once.

--- ps7/2-diet/main.cpp
+++ ps7/2-diet/main.cpp
@@ -24,30 +24,29 @@ int floor_to_double(const CGAL::Quotient<ET>& x) {
 
 int main() {
   std::ios_base::sync_with_stdio(false);
-  // Ensure non-negative quantities per food item.
-  Program lp (CGAL::LARGER, true, 0, false, 0);
   int min, max, n_nutrients, n_foods, price, nutrient_value;
   std::cin >> n_nutrients >> n_foods;
   // We are assured that n_nutrients >= 1 and n_foods >= 1 in a legitimate scenario.
   while (n_nutrients != 0 or n_foods != 0) {
+    // Ensure non-negative quantities per food item.
+    Program lp (CGAL::SMALLER, true, 0, false, 0);
     for (int nutrient_index = 0; nutrient_index < n_nutrients; nutrient_index++) {
       std::cin >> min >> max;
-      lp.set_b(nutrient_index * 2, min);
-      lp.set_b(nutrient_index * 2 + 1, -max);
+      lp.set_b(nutrient_index * 2, -min);
+      lp.set_b(nutrient_index * 2 + 1, max);
     }
     for (int food_index = 0; food_index < n_foods; food_index++) {
       std::cin >> price;
       lp.set_c(food_index, price);
       for (int nutrient_index = 0; nutrient_index < n_nutrients; nutrient_index++) {
         std::cin >> nutrient_value;
-        lp.set_a(food_index, nutrient_index * 2, nutrient_value);
-        lp.set_a(food_index, nutrient_index * 2 + 1, -nutrient_value);
+        lp.set_a(food_index, nutrient_index * 2, -nutrient_value);
+        lp.set_a(food_index, nutrient_index * 2 + 1, nutrient_value);
       }
     }
-    Solution s = CGAL::solve_linear_program(lp, ET());
+    Solution s = CGAL::solve_nonnegative_linear_program(lp, ET());
     if (s.is_optimal()) {
       std::cout << floor_to_double(s.objective_value()) << std::endl;
-      // std::cout << *(s.optimality_certificate_begin()) << std::endl;
     } else {
       std::cout << "No such diet." << std::endl;
     }
