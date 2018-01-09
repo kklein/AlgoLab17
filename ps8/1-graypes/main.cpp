a44a3e9f93aed393118e47f9ba13d2588b500944
kkleindev <kleinkevin.lu@gmail.com>
2018-01-09 16:25:20 +0100

    Solve potw8 1 by using different kernels.

--- ps8/1-graypes/main.cpp
+++ ps8/1-graypes/main.cpp
@@ -1,14 +1,16 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt K;
-typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
+typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt EK;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel IK;
+typedef CGAL::Delaunay_triangulation_2<IK>  Triangulation;
 typedef Triangulation::Edge_iterator  Edge_iterator;
 
-double ceil_to_double(const K::FT& x) {
+double ceil_to_double(const EK::FT& x) {
   double a = std::ceil(CGAL::to_double(x));
   while (a < x) a += 1;
   while (a-1 >= x) a -= 1;
@@ -22,10 +24,10 @@ int main() {
   std::cin >> n;
   while (n > 0) {
     // read points
-    std::vector<K::Point_2> points;
+    std::vector<IK::Point_2> points;
     points.reserve(n);
     for (std::size_t i = 0; i < n; ++i) {
-      K::Point_2 point;
+      IK::Point_2 point;
       std::cin >> point;
       points.push_back(point);
     }
@@ -33,15 +35,16 @@ int main() {
     Triangulation t;
     t.insert(points.begin(), points.end());
 
-    K::FT min_distance = CGAL::squared_distance(points[0], points[1]);
+    IK::FT min_squared_distance = CGAL::squared_distance(points[0], points[1]);
 
     for (Edge_iterator e = t.finite_edges_begin(); e != t.finite_edges_end();
         ++e) {
-      min_distance = std::min(min_distance, t.segment(e).squared_length());
+      min_squared_distance =
+          std::min(min_squared_distance, t.segment(e).squared_length());
     }
 
-    min_distance = 50 * CGAL::sqrt(min_distance);
-    std::cout << ceil_to_double(min_distance) << std::endl;
+    EK::FT min_time = 50 * CGAL::sqrt(min_squared_distance);
+    std::cout << ceil_to_double(min_time) << std::endl;
     std::cin >> n;
   }
 }
