4af1192a77b4cad05aa198577dfee5396ca33a55
kkleindev <kleinkevin.lu@gmail.com>
2018-01-26 19:47:50 +0100

    Refactor ps2 4 as to rely less on map lookups and instead of vector accesses.

--- ps2/4-evolution/main.cpp
+++ ps2/4-evolution/main.cpp
@@ -4,52 +4,57 @@
 #include <string>
 #include <vector>
 
-using namespace std;
+// #include "prettyprint.hpp"
+
+void test_case() {
+  int n_species, n_queries;
+  std::cin >> n_species >> n_queries;
+  std::vector<std::string> names(n_species);
+  std::vector<int> ages(n_species), parents(n_species);
+  std::map<std::string, int> index_loopup;
+  std::string root_name;
+  int max_age = 0;
+  for (int i = 0; i < n_species; i++) {
+    std::cin >> names[i] >> ages[i];
+    index_loopup[names[i]] = i;
+    if (ages[i] > max_age) {
+      max_age = ages[i];
+      root_name = names[i];
+    }
+  }
+
+  for (int i = 0; i < n_species - 1; i++) {
+    std::string child, parent;
+    std::cin >> child >> parent;
+    parents[index_loopup[child]] = index_loopup[parent];
+  }
+  parents[index_loopup[root_name]] = -1;
+
+  for (int i = 0; i < n_queries; i++) {
+    std::string target_name;
+    int target_age;
+    std::cin >> target_name >> target_age;
+    if (target_age >= max_age) {
+      std::cout << root_name << " ";
+    } else {
+      int previous_species;
+      int current_species = index_loopup[target_name];
+      while (ages[current_species] <= target_age and (current_species != -1)) {
+        previous_species = current_species;
+        current_species = parents[current_species];
+      }
+      std::cout << names[previous_species] << ' ';
+    }
+  }
+  std::cout << std::endl;
+}
 
 int main() {
+  std::ios_base::sync_with_stdio(false);
   int n_test_cases;
-  cin >> n_test_cases;
-  for (int test_case = 0; test_case < n_test_cases; test_case++) {
-    int n_species;
-    int n_queries;
-    cin >> n_species;
-    cin >> n_queries;
-    map<string, int> ages;
-    map<string, string> parents;
-    string name;
-    int max_age = 0;
-    string root_name;
-    for (int i = 0; i < n_species; i++) {
-      cin >> name;
-      cin >> ages[name];
-      if (ages[name] > max_age) {
-        max_age = ages[name];
-        root_name = name;
-      }
-    }
-    for (int i = 0; i < n_species - 1; i++) {
-      cin >> name;
-      cin >> parents[name];
-    }
-    parents[root_name] = "stop";
-    for (int i = 0; i < n_queries; i++) {
-      string target_name;
-      cin >> target_name;
-      int target_age;
-      cin >> target_age;
-      if (target_age >= max_age) {
-        cout << root_name << " ";
-      } else {
-        string previous_name;
-        string current_name = target_name;
-        while (ages[current_name] <= target_age and (current_name != "stop")) {
-          previous_name = current_name;
-          current_name = parents[current_name];
-        }
-        cout << previous_name << ' ';
-      }
-    }
-    cout << endl;
+  std::cin >> n_test_cases;
+  for (int test_index = 0; test_index < n_test_cases; test_index++) {
+    test_case();
   }
   return 0;
 }
