f4d4e10fff304801719ee709dfe1db562461f91a
kkleindev <kleinkevin.lu@gmail.com>
2018-01-26 22:28:31 +0100

    Solve ps2 4, 100/100.

--- ps2/4-evolution/main.cpp
+++ ps2/4-evolution/main.cpp
@@ -1,49 +1,80 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <vector>
 
-// #include "prettyprint.hpp"
+int bs(int target_age, std::vector<int> &path, std::vector<int> &ages) {
+  int l_min = -1;
+  int l_max = path.size() - 1;
+  while (l_min + 1 < l_max) {
+    int l = (l_max + l_min) / 2;
+    if (ages[path[l]] <= target_age) {
+      l_max = l;
+    } else {
+      l_min = l;
+    }
+  }
+  return path[l_max];
+}
+
+void dfsbs(int species, std::vector<int> &path,
+    std::vector<std::vector<std::pair<int, int> > > &queries,
+    std::vector<int> &results, std::vector<std::vector<int> > &children, std::vector<int> &ages) {
+  for (std::pair<int, int> query: queries[species]) {
+    results[query.second] = bs(query.first, path, ages);
+  }
+  for (int child: children[species]) {
+    path.push_back(child);
+    dfsbs(child, path, queries, results, children, ages);
+  }
+  path.pop_back();
+}
 
 void test_case() {
-  int n_species, n_queries;
+  int n_species, n_queries, root_index;
   std::cin >> n_species >> n_queries;
   std::vector<std::string> names(n_species);
-  std::vector<int> ages(n_species), parents(n_species);
-  std::map<std::string, int> index_loopup;
+  std::vector<int> ages(n_species);
+  std::vector<std::vector<int> > children(n_species);
+  std::unordered_map<std::string, int> index_lookup;
   std::string root_name;
   int max_age = 0;
   for (int i = 0; i < n_species; i++) {
     std::cin >> names[i] >> ages[i];
-    index_loopup[names[i]] = i;
+    index_lookup[names[i]] = i;
     if (ages[i] > max_age) {
       max_age = ages[i];
       root_name = names[i];
     }
   }
+  root_index = index_lookup[root_name];
 
   for (int i = 0; i < n_species - 1; i++) {
     std::string child, parent;
     std::cin >> child >> parent;
-    parents[index_loopup[child]] = index_loopup[parent];
+    int child_index = index_lookup[child];
+    int parent_index = index_lookup[parent];
+    children[parent_index].push_back(child_index);
   }
-  parents[index_loopup[root_name]] = -1;
 
+  std::vector<std::vector<std::pair<int, int> > > queries(n_species);
+  std::vector<int> results(n_queries);
   for (int i = 0; i < n_queries; i++) {
     std::string target_name;
     int target_age;
     std::cin >> target_name >> target_age;
-    if (target_age >= max_age) {
-      std::cout << root_name << " ";
-    } else {
-      int previous_species;
-      int current_species = index_loopup[target_name];
-      while (ages[current_species] <= target_age and (current_species != -1)) {
-        previous_species = current_species;
-        current_species = parents[current_species];
-      }
-      std::cout << names[previous_species] << ' ';
+    queries[index_lookup[target_name]].push_back(std::make_pair(target_age, i));
+  }
+
+  std::vector<int> path;
+  path.push_back(root_index);
+  dfsbs(root_index, path, queries, results, children, ages);
+
+  for (int i = 0; i < n_queries; i++) {
+    std::cout << names[results[i]];
+    if (i < n_queries - 1) {
+      std::cout << " ";
     }
   }
   std::cout << std::endl;
