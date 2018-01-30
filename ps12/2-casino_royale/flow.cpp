c9b54240ac8bf06a44180cf1ccb8d06588354d3b
kkleindev <kleinkevin.lu@gmail.com>
2018-01-30 12:19:48 +0100

    Solve ps12 2, 100/100.

---
+++ ps12/2-casino_royale/flow.cpp
@@ -0,0 +1,110 @@
+// ALGOLAB BGL Tutorial 3
+// Code snippets demonstrating
+// - MinCostMaxFlow with arbitrary edge costs using cycle_canceling
+// - MinCostMaxFlow with non-negative edge costs using successive_shortest_path_nonnegative_weights
+
+// Compile and run with one of the following:
+// g++ -std=c++11 -O2 bgl_mincostmaxflow.cpp -o bgl_mincostmaxflow; ./bgl_mincostmaxflow
+// g++ -std=c++11 -O2 -I path/to/boost_1_58_0 bgl_mincostmaxflow.cpp -o bgl_mincostmaxflow; ./bgl_mincostmaxflow
+
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <cstdlib>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+
+// BGL Graph definitions
+// =====================
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, Traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > Graph; // new!
+// Interior Property Maps
+typedef boost::property_map<Graph, boost::edge_capacity_t>::type      EdgeCapacityMap;
+typedef boost::property_map<Graph, boost::edge_weight_t >::type       EdgeWeightMap; // new!
+typedef boost::property_map<Graph, boost::edge_residual_capacity_t>::type ResidualCapacityMap;
+typedef boost::property_map<Graph, boost::edge_reverse_t>::type       ReverseEdgeMap;
+typedef boost::graph_traits<Graph>::vertex_descriptor          Vertex;
+typedef boost::graph_traits<Graph>::edge_descriptor            Edge;
+typedef boost::graph_traits<Graph>::out_edge_iterator  OutEdgeIt; // Iterator
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map, weight map and reverse edge map
+// ===============================================================
+class EdgeAdder {
+    Graph &G;
+    EdgeCapacityMap &capacitymap;
+    EdgeWeightMap &weightmap;
+    ReverseEdgeMap  &revedgemap;
+
+public:
+    EdgeAdder(Graph &G, EdgeCapacityMap &capacitymap, EdgeWeightMap &weightmap, ReverseEdgeMap &revedgemap)
+        : G(G), capacitymap(capacitymap), weightmap(weightmap), revedgemap(revedgemap) {}
+
+    void addEdge(int u, int v, long c, long w) {
+        Edge e, rev_e;
+        boost::tie(e, boost::tuples::ignore) = boost::add_edge(u, v, G);
+        boost::tie(rev_e, boost::tuples::ignore) = boost::add_edge(v, u, G);
+        capacitymap[e] = c;
+        weightmap[e] = w; // new!
+        capacitymap[rev_e] = 0;
+        weightmap[rev_e] = -w; // new
+        revedgemap[e] = rev_e;
+        revedgemap[rev_e] = e;
+    }
+};
+
+// The key insight to solving this problem is that one fixes the flow to be
+// the capacity of the train. Hence no matter, what legitimate constellation
+// one selects, the flow will equal the capcity. Hence the minimization tackles
+// the negative priorities that are part of the mission selection.
+
+void test_case() {
+  int n_stops, n_missions, capacity;
+  std::cin >> n_stops >> n_missions >> capacity;
+
+  Graph G(n_stops);
+  EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
+  EdgeWeightMap weightmap = boost::get(boost::edge_weight, G);
+  ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
+  ResidualCapacityMap rescapacitymap = boost::get(boost::edge_residual_capacity, G);
+  EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+
+  Vertex source = boost::add_vertex(G);
+  eaG.addEdge(source, 0, capacity, 0);
+
+  for (int stop_index = 0; stop_index < n_stops - 1; stop_index++) {
+    eaG.addEdge(stop_index, stop_index + 1, capacity, 150);
+  }
+
+  for (int mission_index = 0; mission_index < n_missions; mission_index++) {
+    int stop_a, stop_b, priority;
+    std::cin >> stop_a >> stop_b >> priority;
+    eaG.addEdge(stop_a, stop_b, 1, (stop_b - stop_a) * 150 - priority);
+  }
+
+  boost::successive_shortest_path_nonnegative_weights(G, source, n_stops - 1);
+  int cost = boost::find_flow_cost(G);
+
+  std::cout << 150 * capacity * (n_stops - 1) - cost << std::endl;
+
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  int n_test_cases;
+  std::cin >> n_test_cases;
+  for (int test_index = 0; test_index < n_test_cases; test_index++) {
+    test_case();
+  }
+  return 0;
+}
